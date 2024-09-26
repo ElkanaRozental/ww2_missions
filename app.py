1890ffe347cc81eadb69dedec70e35606bbd3a85
ElkanaRozental <rozentlen@gmail.com>
2024-09-26 15:09:10 +0300

    add insert function to app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 from controller.normalized_mission_controller import mission_blueprint
 from model import NormalizedMission
-from repository.database import create_tables
+from repository.database import create_tables, insert_data_from_unnormalized
 
 
 def create_app():
@@ -14,6 +14,7 @@ def create_app():
 
 if __name__ == '__main__':
     create_tables()
+    insert_data_from_unnormalized()
     app = create_app()
     app.register_blueprint(mission_blueprint, url_prefix="/api/missions")
     app.run(debug=True, use_reloader=False)
